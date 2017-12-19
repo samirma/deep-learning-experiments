ab870ba7f6bacef06a1890f592d11519d1214dd3
Samir Antonio <samir.ma@gmail.com>
2017-12-19 22:00:59 +0000

    feat: mock trade reading from file

--- trade_env.py
+++ trade_env.py
@@ -1,32 +1,32 @@
 import numpy as np
 
+_actions = {
+    'hold': 0,
+    'buy': 1,
+    'sell': 2,
+    'cancel_buy': 3,
+    'cancel_sell': 4
+}
+
+_positions = {
+    'flat': 0,
+    'ordened_sell': 1,
+    'ordened_buy': 2,
+    'bought': 3
+}
+
+_allowed = {
+    'flat': {'hold', 'buy'},
+    'bought': {'hold', 'sell'},
+    'ordened_sell': {'hold', 'cancel_sell'},
+    'ordened_buy': {'hold', 'cancel_buy'}
+}
+
 class TraderEnv():
     """Class for a discrete (buy/hold/sell) spread trading environment.
     """
 
-    _actions = {
-        'hold': 0,
-        'buy': 1,
-        'sell': 2,
-        'cancel_buy': 3,
-        'cancel_sell': 4
-    }
-
-    _positions = {
-        'flat': 0,
-        'ordened_sell': 1,
-        'ordened_buy': 2,
-        'bought': 3
-    }
-    
-    _allowed = {
-        _positions['flat']: {'hold', 'buy'},
-        _positions['bought']: {'hold', 'sell'},
-        _positions['ordened_sell']: {'hold', 'cancel_sell'},
-        _positions['ordened_buy']: {'hold', 'cancel_buy'}
-    }
-
-    def __init__(self, data_generator, spread_coefficients, episode_length=1000, trading_fee=0, time_fee=0, history_length=2):
+    def __init__(self, data_generator, episode_length=1000, trading_fee=0, time_fee=0, history_length=2):
         """Initialisation function
         Args:
             data_generator (tgym.core.DataGenerator): A data
@@ -40,11 +40,10 @@ class TraderEnv():
             history_length (int): number of historical states to stack in the
                 observation vector.
         """
-
+                
         #assert data_generator.n_products == len(spread_coefficients)
         assert history_length > 0
         self._data_generator = data_generator
-        self._spread_coefficients = spread_coefficients
         self._first_render = True
         self._trading_fee = trading_fee
         self._time_fee = time_fee
@@ -63,16 +62,12 @@ class TraderEnv():
         self._data_generator.rewind()
         self._total_reward = 0
         self._total_pnl = 0
-        self._position = self._positions['flat']
+        self._position = _positions['flat']
         self._entry_price = 0
         self._exit_price = 0
 
-        for i in range(self._history_length):
-            self._prices_history.append(next(self._data_generator))
-
-        observation = self._get_observation()
-        self.state_shape = observation.shape
-        self._action = self._actions['hold']
+        observation = self.get_observation()
+        self._action = _actions['hold']
         return observation
 
     def step(self, action):
@@ -96,15 +91,14 @@ class TraderEnv():
         
         observation = self.get_observation()
         
-        if (is_valid(action, self._position):
-
-            if action == self._actions['buy']:
+        if is_valid(action, self._position):
+            if action == _actions['buy']:
                 self.send_order_to_buy()
-            elif action == self._actions['sell']:
+            elif action == _actions['sell']:
                 self.send_order_to_sell()
-            elif action == self._actions['cancel_buy']:
+            elif action == _actions['cancel_buy']:
                 self.cancel_buy()
-            elif action == self._actions['cancel_sell']:
+            elif action == _actions['cancel_sell']:
                 self.cancel_sell()
             
         else:
@@ -131,14 +125,6 @@ class TraderEnv():
     def _handle_close(self, evt):
         self._closed_plot = True
             
-    def is_valid(self, action, position):
-        actions_allowed = _allowed[position]
-        allowed = False
-        for x in actions_allowed:
-            if _actions[x] == action:
-                allowed = True
-        return allowed
-            
     def get_order_value(self):
         ask = self.current["asks"][0]
         bid = self.current["bids"][0]
@@ -158,20 +144,24 @@ class TraderEnv():
     def cancel_sell(self):
         self._position = self._positions['flat']
 
+    def get_current_state(self):
+        return self.current
+        
     def get_observation(self):
-        self.current = self._data_generator(index)
+        self.current = self._data_generator.next()
         current_price = self.current["price"]
         #Checking for passive position changes
-        if self._position == self._positions['ordened_sell'] and current_price >= self._exit_price:
+        if self._position == _positions['ordened_sell'] and current_price >= self._exit_price:
             reward -= self._trading_fee
             self._position = self._positions['flat']
             self.instant_pnl = self._exit_price - self._entry_price
             self._entry_price = 0
-        elif all(self._position == self._positions['ordened_buy']) and current_price <= self._entry_price:
+        elif self._position == _positions['ordened_buy'] and current_price <= self._entry_price:
             reward -= self._trading_fee
-            self._position = self._positions['bought']
+            self._position = _positions['bought']
 
     def get_output_state(self):
         return self.current
-
+            
+            
             
